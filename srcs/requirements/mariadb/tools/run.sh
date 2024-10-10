86d3b214a801e08d89d3519785b5bfe1f01ead18
Nadege Moisan <namoisan@c1r8p2.42quebec.com>
2024-10-10 15:01:52 -0400

    fix

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-#db_name = Database Name
-#db_user = User
-#db_pwd = User Password
-
-service mysql start;
+service mysql start
 mysql -e "CREATE DATABASE IF NOT EXISTS \'${SQL_DATABASE}';"
 mysql -e "CREATE USER IF NOT EXISTS \'${SQL_USER}\'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
