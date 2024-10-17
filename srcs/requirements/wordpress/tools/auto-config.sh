8f721d4cc4b252e7c9e4d4250b564c6c6446ffc1
Nadege Moisan <namoisan@c1r8p2.42quebec.com>
2024-10-17 15:30:23 -0400

    essaie de fix

--- srcs/requirements/wordpress/tools/auto-config.sh
+++ srcs/requirements/wordpress/tools/auto-config.sh
@@ -27,23 +27,23 @@ cd /var/www/html
 
 # if [ ! -f /var/www/html/wp-config.php ]
 # then
-	wp core download --path='/var/www/html' --allow-root
+	wp core download --allow-root
 	wp config create	--allow-root \
 						--dbname=$SQL_DATABASE \
 						--dbuser=$SQL_USER \
 						--dbpass=$SQL_PASSWORD \
-						--dbhost=mariadb:3306 --path='/var/www/html' \
-						--skip-check --allow-root
-	wp core install --path='/var/www/html' \
-					--url='namoisan.42.fr' \
-					--title=$WP_TITLE \
-					--admin_user=$WP_USER \
-					--admin_password=$WP_PASSWORD \
-					--admin_email=$WP_EMAIL \
-					--skip-email --allow-root
-	wp theme install teluro --path='/var/www/html' --activate --allow-root
-	wp user create "${WP_USER}" "${WP_EMAIL}" \
-					--role=author --user_pass="${WP_PASSWORD}" --allow-root
+						--dbhost=mariadb:3306 \
+						--skip-check
+	# wp core install --path='/var/www/html' \
+	# 				--url='namoisan.42.fr' \
+	# 				--title=$WP_TITLE \
+	# 				--admin_user=$WP_USER \
+	# 				--admin_password=$WP_PASSWORD \
+	# 				--admin_email=$WP_EMAIL \
+	# 				--skip-email --allow-root
+	# wp theme install teluro --path='/var/www/html' --activate --allow-root
+	# wp user create "${WP_USER}" "${WP_EMAIL}" \
+	# 				--role=author --user_pass="${WP_PASSWORD}" --allow-root
 # else
 # 	echo "Wordpress already configured."
 # fi
