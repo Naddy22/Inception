96b913408af998027a5816d6c92d66be11f39223
Nadege Moisan <namoisan@c1r5p1.42quebec.com>
2024-10-24 10:47:08 -0400

    menage de fin de projet, essaie de remettre mon ancien scrypte car j'ai des doutes que ce soit la cause

--- srcs/requirements/wordpress/tools/auto-config.sh
+++ srcs/requirements/wordpress/tools/auto-config.sh
@@ -46,31 +46,3 @@ else
 fi
 
 /usr/sbin/php-fpm7.4 -F
-
-
-
-
-# cd /var/www/html
-
-# # Check if WordPress is already downloaded
-# if [ ! -f wp-config.php ]; then
-#     echo "Downloading wp-cli..."
-#     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-#     chmod +x wp-cli.phar
-
-#     # Downloads WordPress core files."
-#     ./wp-cli.phar core download --allow-root
-
-#     # Creates wp-config.php"
-#     ./wp-cli.phar config create --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --allow-root
-
-#     # auto-installs WordPress.
-#     ./wp-cli.phar core install --url="namoisan.42.fr" --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --allow-root
-
-# else
-#     echo "WordPress is already installed. Skipping download and configuration."
-# fi
-
-#     ./wp-cli.phar user create $WP_USER $WP_EMAIL --user_pass=$WP_PASSWORD --role=author --allow-root
-
-# php-fpm7.4 -F
